bd50728adf1b9a4b1ab563667a017374816b06dd
Pavel Astral <astrasourcenext@gmail.com>
2021-10-28 15:37:44 +0700

    more modules i guess

--- App.js
+++ App.js
@@ -1,12 +1,15 @@
-import { StatusBar } from 'expo-status-bar';
 import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
+import { HomeScreen } from './components/HomeScreen';
+import { NavigationContainer } from '@react-navigation/native';
+import { createNativeStackNavigator } from '@react-navigation/native-stack';
+import { Tasks } from './components/Tasks';
 
 export default function App() {
   return (
     <View style={styles.container}>
       <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+      <HomeScreen/>
     </View>
   );
 }
