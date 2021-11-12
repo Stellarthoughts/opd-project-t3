86e58a5e65f5c5e0add1a49686ff7e9e4eea085f
Pavel Astral <astrasourcenext@gmail.com>
2021-11-12 18:50:02 +0700

    deleted styles

--- components/common/Header.js
+++ components/common/Header.js
@@ -77,33 +77,6 @@ const styles = StyleSheet.create({
         width: 40,
         height: 40,
     },
-
-    textTitle: {
-        color: "#eee7e7",
-        fontSize: 22,
-    },
-
-    addButton: {
-        alignItems: 'center',
-        justifyContent: 'center',
-        width: 35,
-        height: 35,
-        backgroundColor: "#8fc998",
-        borderRadius: 20,
-    },
-
-    menuButton: {
-        alignItems: 'center',
-        justifyContent: 'center',
-        width: 35,
-        height: 35,
-        backgroundColor: "#c2b5b5",
-        borderRadius: 2,
-    },
-
-    textButton: {
-        fontSize: 22,
-    }
 });
 
 export default Header;
