3ad90fde5b69157ce26ee3d2ee06232250d98c33
Aleksander Bekrenev <praise2001@yandex.ru>
2021-12-09 01:45:08 +0700

    покрашен statusbar

--- App.js
+++ App.js
@@ -13,9 +13,18 @@ import ScheduleScreen from './components/tasks/schedule/ScheduleScreen';
 
 const Stack = createNativeStackNavigator();
 
+const MyStatusBar = ({backgroundColor, ...props}) => (
+	<View style={[styles.statusBar, { backgroundColor }]}>
+	  <SafeAreaView>
+		<StatusBar translucent backgroundColor={backgroundColor} {...props} />
+	  </SafeAreaView>
+	</View>
+);
+
 function App() {
 	return (
 		<NavigationContainer style={styles.container}>
+			<MyStatusBar backgroundColor="#05CEB6" barStyle="light-content" />
 			<Stack.Navigator initialRouteName="Home">
 				<Stack.Group screenOptions={{ headerShown: false }}>
 					<Stack.Screen name="Tasks" component={TasksScreen}/>
@@ -29,6 +38,9 @@ function App() {
 	);
 }
 
+const STATUSBAR_HEIGHT = StatusBar.currentHeight;
+const APPBAR_HEIGHT = Platform.OS === 'ios' ? 44 : 56;
+
 const styles = StyleSheet.create({
 	container: {
 		flex: 1,
@@ -37,6 +49,13 @@ const styles = StyleSheet.create({
 		justifyContent: 'center',
 		marginTop: Platform.OS == "android" ? StatusBar.currentHeight : 0
 	},
+	statusBar: {
+		height: STATUSBAR_HEIGHT,
+	},
+	appBar: {
+		backgroundColor:'#05CEB6',
+		height: APPBAR_HEIGHT,
+	}
 });
 
 export default App;
