0ad9434a04cb9f13407bd5e106d3484a0f7d86a4
Daniil <bezginov.daniil@mail.ru>
2021-12-18 17:03:31 +0700

    Страничка сортировки подзадач на неделю
    Почти доделал, нужно как то передать функцию Uppdate с WeekItem в TaskSortedByWeekScreen и тогда при перелистывании подзадачи будут обновляться

--- App.js
+++ App.js
@@ -8,7 +8,7 @@ import TasksScreen from './components/tasks/untimed/TasksScreen';
 import { SafeAreaView } from 'react-native-safe-area-context';
 import SettingsScreen from './components/settings/SettingsScreen';
 import HabitsScreen from './components/habits/HabitsScreen';
-import TasksTimedScreen from './components/tasks/timed/TasksTimedScreen';
+import TasksTimedScreen from './components/tasks/timed/sortedByWeek/TaskSortedByWeekScreen';
 import ScheduleScreen from './components/tasks/schedule/ScheduleScreen';
 
 const Stack = createNativeStackNavigator();
---
+++ components/tasks/timed/sortedByWeek/WeekItem.js
@@ -0,0 +1,46 @@
+import React, {useState, useEffect, useRef} from 'react';
+import { StyleSheet, View, FlatList, Text, Image, Animated, TextInput, CheckBox, Button, ScrollView } from 'react-native';
+import CButton from '../../../common/CButton';
+import SubtaskList from '../../SubtaskList';
+
+function WeekItem({ el, updateHandler }) {
+    const [listSubtask, setListSubtask] = useState(el.subtasksItem);
+
+    // Обновление
+    const Update = (data) => {
+        setListSubtask((list) => data);
+    }
+
+    // Форматирование вывода
+    const formatDateWeek = () => {
+        var date = el.date.getDate();
+        var month = el.date.getMonth() + 1;
+        if (date < 10) date = "0" + date;
+        if (month < 10) month = "0" + month;
+        return el.shortName + " " + date + "." + month;
+    }
+
+    // Вывод элемента
+    return (
+        <View style={styles.container}>
+            <Text  style={styles.title}>{formatDateWeek()}</Text>
+            <SubtaskList data={listSubtask} set={setListSubtask} updateHandler={updateHandler}></SubtaskList>
+            <CButton onPress={() => Update([{name: "Задача 1", key: Math.random().toString(36).substring(7), done: false},{name: "Задача 1", key: Math.random().toString(36).substring(7), done: false}])}/>
+        </View>
+    );
+}
+
+const styles = StyleSheet.create({
+    container: {
+        width: "100%",
+        padding: 15
+    },
+
+    title: {
+        color: "#0AB09C",
+        fontSize: 16,
+        marginBottom: 10
+    }
+});
+
+export default WeekItem;
